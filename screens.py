46cbf2bb7ba2f45287eec0e688edea1b056acc78
Ryan Kortbeek <ryankbeek@gmail.com>
2020-10-26 15:17:34 -0600

    added cool start page and updated screen appearances

--- screens.py
+++ screens.py
@@ -48,9 +48,26 @@ class StartScreen(BaseScreen):
         """
         Prints out the screen title and the options supported by this screen.
         """
-        print('SELECT USER TYPE')
-        print('Please enter the number corresponding to the type of user that you are:')
-        print('\t[1] Registered User\n\t[2] Unregistered User\n')
+        start_msg = '''WELCOME TO___________________________________________________
+                                                            /
+       ____________________________________                /
+      /  ___   /  ___   /  _______/  _____/               /
+     /  /__/  /  /__/  /  / _____/  /__                  /
+    /  ______/  ___   /  / /_   /  ___/                 /
+   /  /     /  /  /  /  /___/  /  /____                /
+  /__/     /__/__/__/_________/_______/____  ___      /
+          /  __  / /  _____  /  ____   /  / /  /     /
+         /  /_/ /_/  /   /  /  /   /  /  /_/  /_    /
+        /  ___   /  /   /  /  /   /  /  ____   /   /
+       /  /__/  /  /___/  /  /___/  /  /   /  /   /
+      /________/_________/_________/__/   /__/   /
+                                                /
+_______________________________________________/
+
+Please select the type of user that you are:
+\t[1] registered user
+\t[2] unregistered user'''
+        print(start_msg)
 
     def run(self):
         """
@@ -58,10 +75,11 @@ class StartScreen(BaseScreen):
         :return: an integer corresponding to the user's type
         """
         valid_inputs = ['1', '2']
-        user_type = input()
+        user_type = input('> ')
         while user_type not in valid_inputs:
-            user_type = input('"{}" is an invalid selection, please enter a valid selection from the menu above.\n'.
-                              format(user_type))
+            print('"{}" is an invalid selection, please enter a valid selection from the menu above.'
+                  .format(user_type))
+            user_type = input('> ')
         return user_type
 
 
@@ -116,13 +134,17 @@ class LoginScreen(BaseScreen):
         continues and the uid of the logged in user is returned.
         :return: the uid of the logged in user
         """
-        login_uid = input('Please enter your user id (max 4 characters):\n')
+        print('\nPlease enter your user id (max 4 characters):')
+        login_uid = input('> ')
         while (len(login_uid) > 4) or (not self._login_uid_exists(login_uid)):
-            login_uid = input('Invalid user id, try again:\n')
-        login_pwd = input('Please enter your password:\n')
+            print('Invalid user id, try again:')
+            login_uid = input('> ')
+        print('\nPlease enter your password:')
+        login_pwd = input('> ')
         while not self._pwd_is_correct(login_uid, login_pwd):
-            login_pwd = input('Incorrect password, try again:\n')
-        print('Login Successful')
+            print('Incorrect password, try again:')
+            login_pwd = input('> ')
+        print('\nLogin Successful')
         sleep(0.5)
         return login_uid
 
@@ -161,15 +183,20 @@ class SignUpScreen(BaseScreen):
         continues, the user is logged in, and the uid of the new logged in user is returned.
         :return: the uid of the logged in user
         """
-        new_uid = input('Please enter a user id that you would like to use (max 4 characters):\n')
+        print('\nPlease enter a user id that you would like to use (max 4 characters):')
+        new_uid = input('> ')
         while (len(new_uid) > 4) or (self._uid_already_exists(new_uid)):
-            new_uid = input('The entered user id either has more than 4 characters or already exists, try again:\n')
-        name = input('Please enter your name:\n')
-        city = input('Please enter the name of the city you live in:\n')
-        pwd = input('Please enter the password you would like to use:\n')
+            print('The entered user id either has more than 4 characters or already exists, try again:')
+            new_uid = input('> ')
+        print('\nPlease enter your name:')
+        name = input('> ')
+        print('\nPlease enter the name of the city you live in:')
+        city = input('> ')
+        print('\nPlease enter the password you would like to use:')
+        pwd = input('> ')
         insertion = 'insert into users values (:new_uid, :name, :pwd, :city, date());'
         self.cursor.execute(insertion, {'new_uid': new_uid, 'name': name, 'pwd': pwd, 'city': city})
         self.connection.commit()
-        print('Sign Up Successful - you will now be logged in')
+        print('\nSign Up Successful - you will now be logged in')
         sleep(0.5)
         return new_uid
