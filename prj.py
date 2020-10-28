ce0b1568dfda77d244f22a68da6de9c5baf6b71b
ryankortbeek <rakortbe@ualberta.ca>
2020-10-28 16:33:53 -0600

    feature: refactored and implemented main menu flow

--- prj.py
+++ prj.py
@@ -12,24 +12,49 @@ class PostBook:
 
     def __init__(self, db_path):
         """
-        Gets a connection to the database at db_path and runs the program.
+        Gets a connection to the database at db_path and initializes so this program can be run.
         :param db_path: command line argument specifying the path to the database this program is to run on
         """
-        self.connection = None
-        self.cursor = None
+        self.current_user = None
+        self.running = True
         self.db_manager = DBManager(db_path)
-        self._run()
 
-    def _run(self):
+    def _run_login(self):
+        """
+        Runs the login process - user is first taken to the start screen where they are prompted to select the type
+        of user that they are (i.e. registered or unregistered), and then are taken to either the LOGIN or SIGN UP
+        screen. Upon successfully signing up (this also logs the user in) or logging in this function returns the uid
+        of the logged in user.
+        :return: the uid of the logged in user as a string
+        """
+        start_screen = StartScreen()
+        user_type = start_screen.run()
+        login_screen = LoginScreen(self.db_manager) if user_type == 'registered' else SignUpScreen(self.db_manager)
+        return login_screen.run()
+
+    def run(self):
         """
         Runs the program.
         """
-        user_select_screen = StartScreen()
-        user_type = user_select_screen.run()
-        login_screen = LoginScreen(self.db_manager) if user_type == '1' else SignUpScreen(self.db_manager)
-        logged_in_uid = login_screen.run()
-        # TODO: implement the main menu
-        self.connection.close()
+        self.current_user = self._run_login()
+        while self.running:
+            if self.current_user is None:
+                self.current_user = self._run_login()
+            mm_screen = MainMenuScreen(self.current_user)
+            task = mm_screen.run()
+            if task == 'post question':
+                # TODO implement post question functionality
+                pass
+            elif task == 'search':
+                # TODO implement search functionality
+                pass
+            elif task == 'logout':
+                self.current_user = None
+            # Happens when task == 'exit'
+            else:
+                self.running = False
+        self.db_manager.close_connection()
+        clear_screen()
 
 
 def main():
@@ -37,6 +62,7 @@ def main():
                                  '"python3 prj.py PATH_TO_DATABASE"'
     assert path.exists(sys.argv[1]), 'path does not exist - please specify a valid path'
     p = PostBook(sys.argv[1])
+    p.run()
 
 
 if __name__ == '__main__':
